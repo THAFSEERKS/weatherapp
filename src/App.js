a20b43352049b53f0b01a17e5d41e3badcf826b5
THAFSEERKS <muhammedthafseer.mec@gmail.com>
2024-07-12 16:21:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Weather from "./components/Weather"
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <div >
+        <Weather/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
